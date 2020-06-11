fcef2762f63a54dbb0b0e8053693d76cd4e4ca7f
Chris Lu <luchris@google.com>
2020-06-11 22:55:54 +0800

    hwc2: add locks for callback functions
    In case surfaceflinger is killed, the function pointer of
    RegisterCallback will be null. If the system is still handling
    callback function tasks, it could have the chance to access null
    pointer and result in a crash. This change adds locks for
    callback functions to ensure the tasks are finished then we can
    assign null pointer via RegisterCallback.
    
    Bug: 150223963
    Test: no crash while surfaceflinger is killed
    Change-Id: I9ae8ac8968f4ff9286846de15df0411b5d79ae63

--- sdm/libs/hwc2/hwc_callbacks.h
+++ sdm/libs/hwc2/hwc_callbacks.h
@@ -36,6 +36,8 @@
 #undef HWC2_INCLUDE_STRINGIFICATION
 #undef HWC2_USE_CPP11
 
+#include <mutex>
+
 namespace sdm {
 
 class HWCCallbacks {
@@ -69,6 +71,10 @@ class HWCCallbacks {
   HWC2_PFN_VSYNC vsync_ = nullptr;
   hwc2_display_t vsync_source_ = HWC_DISPLAY_PRIMARY;   // hw vsync is active on this display
   std::bitset<kNumDisplays> pending_refresh_;         // Displays waiting to get refreshed
+
+  std::mutex hotplug_mutex_;
+  std::mutex refresh_mutex_;
+  std::mutex vsync_mutex_;
 };
 
 }  // namespace sdm
